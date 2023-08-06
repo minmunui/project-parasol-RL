476e48da9a19487c88bfda0e4fd2bb9de6b05d32
Dongjin <ehdwls1638@gmail.com>
2023-08-06 13:52:16 +0900

    feat : add env by long/short

--- test_env.py
+++ test_env.py
@@ -1,4 +1,4 @@
-import src.env.env_bs as env
+import src.env.env_ls as env
 import src.utils.utils as utils
 
 urls = './data/STOCKS_GOOGL.csv'
@@ -7,12 +7,27 @@ stock_info = utils.load_data(urls)
 
 env = env.MyEnv(stock_info)
 
+default_option = {
+    'window_size': 10,
+    'start_index': 0,
+    'end_index': 100,
+    'commission': 0.01,
+    'selling_tax': 0.01,
+}
+
+
 env.reset()
 
-while True:
-    while env._done is False:
-        user_input = input("Enter your action \"\": ")
-        env.step(int(user_input))
-        env.print_current_state()
 
-    env.reset()
+while not env._done:
+    user_input = input("Enter your action [ 0 : short , 1 : long ] : ")
+    if (user_input == '0') or (user_input == '1'):
+        print("done : ", env._done, "")
+        env.step(int(user_input))
+        env.print_history()
+    else:
+        env.print_info()
+
+env.print_info()
+print("Over")
+env.print_history()
