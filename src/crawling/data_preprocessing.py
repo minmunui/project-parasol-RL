aebe86d4f5d8d520b7311dc1e4c5d6bc967a5bc7
Dongjin <ehdwls1638@gmail.com>
2023-09-02 15:10:23 +0900

    feat : quanty data 추가기능 구현

--- src/crawling/data_preprocessing.py
+++ src/crawling/data_preprocessing.py
@@ -1,28 +1,27 @@
 import pandas as pd
 
+def addScalingRateColumn(df, sourceColumn, targetColumn):
+    df[targetColumn] = df[sourceColumn] / df[sourceColumn].shift(1)
+    return df
+
+
+def addMovingAverageRateColumn(df, windowSize, sourceColumn, targetColumn):
+    df[targetColumn] = df[sourceColumn] / df[sourceColumn].rolling(window=windowSize).mean()
+    return df
+
+
+def addQuantyColumns(df, windowSizes, tradingColumn, closeColumn):
+    addScalingRateColumn(df, tradingColumn, "전일대비거래량비율")
+    for windowSize in windowSizes:
+        addMovingAverageRateColumn(df, windowSize, tradingColumn, str(windowSize) + "일평균거래량비율")
+        addMovingAverageRateColumn(df, windowSize, closeColumn, str(windowSize) + "일평균종가비율")
+
+    return df
+
 
 def main():
-    # main은 예시 코드입니다. 아래와 같이 addMax(Min)Column 함수를 사용하면 됩니다.
-    # 예시 데이터 생성 (일자와 종가 컬럼을 갖는 데이터프레임)
-    data = {'일자': ['2023-08-25', '2023-08-24', '2023-08-23', '2023-08-22', '2023-08-21', '2023-08-20'],
-            '종가': [1000, 1020, 1050, 1015, 1035, 1005]}
-
-    df = pd.DataFrame(data)
-    df['일자'] = pd.to_datetime(df['일자'])  # 일자 컬럼을 날짜 형식으로 변환
-    df = df.sort_values(by='일자', ascending=True)  # 일자 기준 오름차순 정렬
-    addMaxColumn(df, 5, '종가', '최근5일최고가')  # 최근 5일 종가의 최고값을 구하여 '최근5일최고가' 컬럼에 추가
+    # 파일 불러오기
+    df = pd.read_csv('data/norm_final_sk_hynix.csv')
     print(df)
-
-
-# dataFrame: 데이터프레임
-# windowSize: 최근 몇일간의 최고(저)가를 구할 것인지
-# sourceColumn: 최고(저)가를 구할 컬럼
-# targetColumn: 최고(저)가를 추가할 컬럼
-def addMaxColumn(dataFrame, windowSize, sourceColumn, targetColumn):
-    dataFrame[targetColumn] = dataFrame[sourceColumn].rolling(window=windowSize).max()
-    return dataFrame
-
-
-def addMinColumn(dataFrame, windowSize, sourceColumn, targetColumn):
-    dataFrame[targetColumn] = dataFrame[sourceColumn].rolling(window=windowSize).min()
-    return dataFrame
+    addQuantyColumns(df, [5, 10, 30, 60, 120], '회전율', 'Close')
+    print(df)
